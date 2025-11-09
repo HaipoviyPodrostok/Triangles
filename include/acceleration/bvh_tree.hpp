d66666d6a57c9146ac0cef2459c94275b451fa34
HaipoviyPodrostok <alexkurteev228@mail.ru>
2025-11-09 21:45:20 +0300

    change partition()

--- include/acceleration/bvh_tree.hpp
+++ include/acceleration/bvh_tree.hpp
@@ -12,13 +12,16 @@ inline constexpr size_t tree_max_depth = 64;
 struct BVHNode {
     AABB box;
    
-    int left  = -1;
-    int right = -1;
+    int left_idx  = -1;
+    int right_idx = -1;
    
-    size_t first  = 0;
+    size_t start  = 0;
     size_t n_objs = 0;
    
     bool is_leaf = false;
+
+    void init_leaf(const AABB& box_, const size_t start_, const size_t n_objs_);
+    void init_internal(const AABB& box_, const int left_idx_, const int right_idx_);
 
     BVHNode(const AABB& box_, const size_t first_, const size_t n_objs_);
 };
@@ -28,12 +31,15 @@ struct BVHTree {
     std::vector<geometry::Triangle> triangles;
 
     int root = -1;
+    size_t max_depth_reached = 0;
 
     BVHTree(const std::vector<geometry::Triangle>& input);
 
-    void partition(const int idx);
-    void sort_triangles_by_centroids(const BVHNode& node, const math::Axis wildest_axis, 
+    AABB calculate_box(const size_t start, const size_t n_objs);
+    void partition_by_median(const int idx);
+    void sort_triangles_by_centers(const BVHNode& node, const math::Axis wildest_axis, 
                                      const size_t mid_idx);
+    int build_node(const size_t start, const size_t n_objs, size_t depth);
 
 };
 } // namespace acceleration
--- source/acceleration/bvh_tree.cpp
+++ source/acceleration/bvh_tree.cpp
@@ -11,9 +11,20 @@
 namespace acceleration {
 
 BVHNode::BVHNode(const AABB& box_, const size_t first_, const size_t n_objs_)
-    : box(box_), first(first_), n_objs(n_objs_) { 
+    : box(box_), start(first_), n_objs(n_objs_) { 
         assert(box_.is_valid());
-    }
+}
+
+void BVHNode::init_leaf(const AABB& box_, const size_t start_, const size_t n_objs_) {
+    box = box_; start = start_; n_objs = n_objs_;
+    left_idx = -1; right_idx = -1;
+}
+
+void BVHNode::init_internal(const AABB& box_, const int left_idx_, const int right_idx_) {
+    box = box_; start = 0; n_objs = 0;
+    left_idx  = left_idx_;
+    right_idx = right_idx_;
+}
 
 BVHTree::BVHTree(const std::vector<geometry::Triangle>& input) 
     : triangles(input) {
@@ -31,80 +42,106 @@ BVHTree::BVHTree(const std::vector<geometry::Triangle>& input)
     BVHNode root_node{world_box, 0, triangles.size()};
     
     nodes.emplace_back(root_node);
-    partition(0);
+    partition_by_median(0);
 }
 
-void BVHTree::partition(const int node_idx) { 
-    BVHNode node = nodes[node_idx];
+AABB BVHTree::calculate_box(const size_t start, const size_t n_objs) {
+    AABB box{triangles[start]};
+    for (size_t i = start + 1; i < start + n_objs; ++i) {
+        box.expand(triangles[i]);
+    }
+    return box;
+}
 
-    if (node.n_objs < max_leaf_capacity) {
+void BVHTree::partition_by_median(const int node_idx) { 
+    BVHNode& node = nodes[node_idx];
+
+    if (node.n_objs <= max_leaf_capacity) {
         node.is_leaf = true;
         return;
     }
     
-    const float spread_x = fabs(nodes[node_idx].box.max.x - nodes[node_idx].box.min.x);
-    const float spread_y = fabs(nodes[node_idx].box.max.y - nodes[node_idx].box.min.y);
-    const float spread_z = fabs(nodes[node_idx].box.max.z - nodes[node_idx].box.min.z);
+    const float spread_x = std::fabs(nodes[node_idx].box.max.x - nodes[node_idx].box.min.x);
+    const float spread_y = std::fabs(nodes[node_idx].box.max.y - nodes[node_idx].box.min.y);
+    const float spread_z = std::fabs(nodes[node_idx].box.max.z - nodes[node_idx].box.min.z);
 
     math::Axis wildest_axis = (spread_x >= spread_y && spread_x >= spread_z) ? math::Axis::X :
                 (spread_y >= spread_z ? math::Axis::Y : math::Axis::Z);
                
-    size_t mid_idx = node.first + node.n_objs / 2;
+    const size_t mid_idx = node.start + node.n_objs / 2;
     
-    sort_triangles_by_centroids(node, wildest_axis, mid_idx);
+    sort_triangles_by_centers(node, wildest_axis, mid_idx);
 
-    AABB left_box{triangles[node.first]};
-    AABB right_box{triangles[mid_idx]};
-    
-    size_t i = node.first;
-
-    for (;i < mid_idx; ++i) {
+    AABB left_box{triangles[node.start]};
+    for (size_t i = node.start + 1; i < mid_idx; ++i) {
         left_box.expand(triangles[i]);
     }
-
-    size_t left_n_objs = mid_idx; //mid_idx -> right box
-
-    for (; i < node.first + node.n_objs; ++i) {
+    
+    AABB right_box{triangles[mid_idx]};  
+    for (size_t i = mid_idx + 1; i < node.start + node.n_objs; ++i) {
         right_box.expand(triangles[i]);
     }
+    
+    const size_t left_n_objs = mid_idx - node.start; //mid_idx -> right box
+    const size_t right_n_objs = node.n_objs - left_n_objs;
 
-    size_t right_n_objs = node.n_objs - left_n_objs;
+    if (left_n_objs == 0 || right_n_objs == 0) {
+        node.is_leaf = true;
+        return;
+    }
 
-    BVHNode left_node{left_box, node.first, left_n_objs};
-    BVHNode right_node{right_box, mid_idx, right_n_objs};
-
-    node.left = nodes.size();
-    nodes.emplace_back(left_node);
-    partition(node.first);       //TODO нормальные имена
-
-    node.right = nodes.size();
-    nodes.emplace_back(right_node);
-    partition(mid_idx);
+    const int left_child_idx = nodes.size();
+    nodes.emplace_back(BVHNode{left_box, node.start, left_n_objs});
+    
+    const int right_child_idx = nodes.size();
+    nodes.emplace_back(BVHNode{right_box, mid_idx, right_n_objs});
+    
+    node.left_idx = left_child_idx;
+    node.right_idx = right_child_idx;
+    
+    partition_by_median(left_child_idx);
+    partition_by_median(right_child_idx);
 }
 
-void BVHTree::sort_triangles_by_centroids(const BVHNode& node, const math::Axis wildest_axis, const size_t mid_idx) {
+void BVHTree::sort_triangles_by_centers(const BVHNode& node, const math::Axis wildest_axis, const size_t mid_idx) {
     switch(wildest_axis) {
         case math::Axis::X: {
-            auto centroid_cmp = [](const geometry::Triangle& a, const geometry::Triangle& b) {
+            auto centre_cmp = [](const geometry::Triangle& a, const geometry::Triangle& b) {
                 return  a.centroid.x < b.centroid.x;
             };
-            std::nth_element(node.first, mid_idx, node.first + node.n_objs, centroid_cmp);
+            std::nth_element(node.start, mid_idx, node.start + node.n_objs, centre_cmp);
         }
         case math::Axis::Y: {
-            auto centroid_cmp = [](const geometry::Triangle& a, const geometry::Triangle& b) {
+            auto centre_cmp = [](const geometry::Triangle& a, const geometry::Triangle& b) {
                 return  a.centroid.y < b.centroid.y;
             };
-            std::nth_element(node.first, mid_idx, node.first + node.n_objs, centroid_cmp);
+            std::nth_element(node.start, mid_idx, node.start + node.n_objs, centre_cmp);
         }
         default: {
-            auto centroid_cmp = [](const geometry::Triangle& a, const geometry::Triangle& b) {
+            auto centre_cmp = [](const geometry::Triangle& a, const geometry::Triangle& b) {
                 return  a.centroid.z < b.centroid.z;
             };
-            std::nth_element(node.first, mid_idx, node.first + node.n_objs, centroid_cmp);
+            std::nth_element(node.start, mid_idx, node.start + node.n_objs, centre_cmp);
         }
     }
 }
 
+int BVHTree::build_node(const size_t start, const size_t n_objs, size_t depth) {
+    int node_idx = nodes.size();
+    nodes.emplace_back();
+
+    if (depth > max_depth_reached) {
+        max_depth_reached = depth;
+    }
+
+    if (n_objs <= max_leaf_capacity || depth >= max_depth_reached) {
+        nodes[node_idx].init_leaf(calculate_box(start, n_objs), start, n_objs);
+        return node_idx;
+    }
+
+    int mid_idx = 
+
+}
 } // namespace acceleration
     
     // geometry::Vector3D node_box_min = nodes[idx].box.min;
