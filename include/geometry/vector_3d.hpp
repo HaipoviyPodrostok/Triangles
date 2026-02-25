b0c5e5a8c5d33197bed35eb26d2ce8c5297b27b5
HaipoviyPodrostok <alexkurteev228@mail.ru>
2026-02-25 06:45:56 +0300

    operators

--- include/geometry/vector_3d.hpp
+++ include/geometry/vector_3d.hpp
@@ -3,40 +3,38 @@
 #include <cassert>
 #include <cmath>
 #include <cstddef>
-#include <math.h>
 #include <gtest/gtest.h>
+#include <math.h>
 
 namespace geometry {
 
 struct Vector3D {
-    double x;
-    double y;
-    double z;
+  double x;
+  double y;
+  double z;
 
-    Vector3D(double x_ = NAN, double y_ = NAN, double z_ = NAN);
+  Vector3D(double x_ = NAN, double y_ = NAN, double z_ = NAN);
 
-    static inline Vector3D invalid() {
-        return {NAN, NAN, NAN};
-    }
-    
-    bool is_valid     ()                      const;
-    bool is_zero      (double scale = 1.0)    const;
-    bool is_collinear (const Vector3D& other) const;
-    bool is_codirected(const Vector3D& other) const;
-    bool is_match     (const Vector3D& other) const;
+  static inline Vector3D invalid() { return {NAN, NAN, NAN}; }
 
-    double length() const;
-    double scalar(const Vector3D& other) const;
+  bool is_valid() const;
+  bool is_zero(double scale = 1.0) const;
+  bool is_collinear(const Vector3D &other) const;
+  bool is_codirected(const Vector3D &other) const;
+  bool is_match(const Vector3D &other) const;
 
-    Vector3D      operator+  (const Vector3D& other) const;
-    Vector3D      operator-  (const Vector3D& other) const;
-    Vector3D      operator*  (double scalar)         const;
-    Vector3D      operator/  (double scalar)         const;
-    double&       operator[] (size_t idx)                 ;
-    const double& operator[] (size_t idx)            const; 
-          
-    Vector3D cross(const Vector3D& other) const;
-    
-    void print() const;
+  double length() const;
+  double scalar(const Vector3D &other) const;
+
+  // Vector3D      operator+  (const Vector3D& other) const;
+  // Vector3D      operator-  (const Vector3D& other) const;
+  // Vector3D      operator*  (double scalar)         const;
+  // Vector3D      operator/  (double scalar)         const;
+  // double&       operator[] (size_t idx)                 ;
+  // const double& operator[] (size_t idx)            const;
+
+  Vector3D cross(const Vector3D &other) const;
+
+  void print() const;
 };
 } // namespace geometry
--- source/geometry/vector_3d.cpp
+++ source/geometry/vector_3d.cpp
@@ -7,101 +7,100 @@
 
 namespace geometry {
 
-Vector3D::Vector3D(double x, double y, double z)
-    : x(x), y(y), z(z) {
-}
+Vector3D::Vector3D(double x, double y, double z) : x(x), y(y), z(z) {}
 
 bool Vector3D::is_valid() const {
-    return std::isfinite(x) && std::isfinite(y) && std::isfinite(z);
+  return std::isfinite(x) && std::isfinite(y) && std::isfinite(z);
 }
 
-Vector3D Vector3D::operator+ (const Vector3D& other) const {
-    assert(this->is_valid());
-    assert(other.is_valid());
-    return Vector3D{x + other.x, y + other.y, z + other.z};
+Vector3D operator+(const Vector3D)
+    // Vector3D Vector3D::operator+ (const Vector3D& other) const {
+    //     assert(this->is_valid());
+    //     assert(other.is_valid());
+    //     return Vector3D{x + other.x, y + other.y, z + other.z};
+    // }
+
+    // Vector3D Vector3D::operator- (const Vector3D& other) const {
+    //     assert(this->is_valid());
+    //     assert(other.is_valid());
+    //     return Vector3D{x - other.x, y - other.y, z - other.z};
+    // }
+
+    // Vector3D Vector3D::operator* (double scalar) const {
+    //     assert(this->is_valid());
+    //     return Vector3D{x * scalar, y * scalar, z * scalar};
+    // }
+
+    // Vector3D Vector3D::operator/ (double scalar) const {
+    //     assert(this->is_valid());
+    //     assert(!math::is_zero(scalar));
+    //     return *this * (1 / scalar);
+    // }
+
+    // double& Vector3D::operator[] (size_t idx) {
+    //     assert(this->is_valid());
+    //     if (idx == 0) { return x; };
+    //     if (idx == 1) { return y; };
+    //     if (idx == 2) { return z; };
+    //     throw std::out_of_range("Vector3D index out of range");;
+    // }
+
+    // const double& Vector3D::operator[] (size_t idx) const {
+    //     assert(this->is_valid());
+    //     if (idx == 0) { return x; };
+    //     if (idx == 1) { return y; };
+    //     if (idx == 2) { return z; };
+    //     throw std::out_of_range("Vector3D index out of range");;
+    // }
+
+    bool Vector3D::is_collinear(const Vector3D &other) const {
+  assert(this->is_valid());
+  assert(other.is_valid());
+
+  const double scale = this->length() * other.length();
+  return ((this->cross(other)).is_zero(scale));
 }
 
-Vector3D Vector3D::operator- (const Vector3D& other) const {
-    assert(this->is_valid());
-    assert(other.is_valid());
-    return Vector3D{x - other.x, y - other.y, z - other.z};
+bool Vector3D::is_codirected(const Vector3D &other) const {
+  assert(this->is_valid());
+  assert(other.is_valid());
+  return (is_collinear(other) && this->scalar(other) >= 0.0f);
 }
 
-Vector3D Vector3D::operator* (double scalar) const {
-    assert(this->is_valid());
-    return Vector3D{x * scalar, y * scalar, z * scalar};
-}
-
-Vector3D Vector3D::operator/ (double scalar) const {
-    assert(this->is_valid());
-    assert(!math::is_zero(scalar));
-    return *this * (1 / scalar);
-}
-
-double& Vector3D::operator[] (size_t idx) {
-    assert(this->is_valid());
-    if (idx == 0) { return x; };
-    if (idx == 1) { return y; };
-    if (idx == 2) { return z; };
-    throw std::out_of_range("Vector3D index out of range");;
-}
-
-const double& Vector3D::operator[] (size_t idx) const {
-    assert(this->is_valid());
-    if (idx == 0) { return x; };
-    if (idx == 1) { return y; };
-    if (idx == 2) { return z; };
-    throw std::out_of_range("Vector3D index out of range");;
-}
-
-bool Vector3D::is_collinear(const Vector3D& other) const {
-    assert(this->is_valid());
-    assert(other.is_valid());
-
-    const double scale = this->length() * other.length();
-    return ( (this->cross(other) ).is_zero(scale) );
-}
-
-bool Vector3D::is_codirected(const Vector3D& other) const {
-    assert(this->is_valid());
-    assert(other.is_valid());
-    return (is_collinear(other) && this->scalar(other) >= 0.0f);
-}
-
-bool Vector3D::is_match(const Vector3D& other) const {
-    assert(this->is_valid());
-    assert(other.is_valid());
-    return ((*this - other).is_zero());
+bool Vector3D::is_match(const Vector3D &other) const {
+  assert(this->is_valid());
+  assert(other.is_valid());
+  return ((*this - other).is_zero());
 }
 
 bool Vector3D::is_zero(double scale) const {
-    assert(this->is_valid());
-    return (math::is_zero(length(), scale)) ;
+  assert(this->is_valid());
+  return (math::is_zero(length(), scale));
 }
 
 double Vector3D::length() const {
-    assert(this->is_valid());
-    return (std::sqrtf(x * x + y * y + z * z));
+  assert(this->is_valid());
+  return (std::sqrtf(x * x + y * y + z * z));
 }
 
-double Vector3D::scalar(const Vector3D& other) const {
-    assert(this->is_valid());
-    assert(other.is_valid());
-    return (x * other.x + y * other.y + z * other.z);
+double Vector3D::scalar(const Vector3D &other) const {
+  assert(this->is_valid());
+  assert(other.is_valid());
+  return (x * other.x + y * other.y + z * other.z);
 }
 
-Vector3D Vector3D::cross(const Vector3D& other) const {
-    assert(this->is_valid());
-    assert(other.is_valid());
- 
-    float i = (y * other.z) - (z * other.y);
-    float j = (z * other.x) - (x * other.z);
-    float k = (x * other.y) - (y * other.x);
-    
-    return Vector3D{i, j, k};
+Vector3D Vector3D::cross(const Vector3D &other) const {
+  assert(this->is_valid());
+  assert(other.is_valid());
+
+  float i = (y * other.z) - (z * other.y);
+  float j = (z * other.x) - (x * other.z);
+  float k = (x * other.y) - (y * other.x);
+
+  return Vector3D{i, j, k};
 }
 
 void Vector3D::print() const {
-    std::cout << "(" << x << ", " << y << ", " << z << ")";
+  std::cout << "(" << x << ", " << y << ", " << z << ")";
 }
 } // namespace geometry
